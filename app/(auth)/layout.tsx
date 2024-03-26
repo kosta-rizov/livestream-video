8d2c157460373383a40b3505ca949a94040307a9
kosta-rizov <kosta.rizov85@gmail.com>
2024-03-26 16:26:56 +0100

    feat: add Dark mode

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,14 @@
 import React, { ReactNode } from 'react'
+import { ThemeProvider } from "@/components/theme-provider"
+import { Logo } from './_components/logo'
 
 
 const AuthLayout = ({children}: {children:ReactNode}) => {
   return (
-    <div className='h-full flex justify-center items-center'>{children}</div>
+    <div className='h-full flex flex-col justify-center items-center space-y-6'>
+      <Logo />
+      {children}
+    </div>
   )
 }
 
