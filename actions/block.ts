b48a6552d8b08beb384a8ad246140f7fc763f6cb
kosta-rizov <kosta.rizov85@gmail.com>
2024-04-16 01:53:35 +0200

    feat: add User Page

--- actions/block.ts
+++ actions/block.ts
@@ -1,25 +1,43 @@
-"use server"
-import { blockUser, unblockUser } from "@/lib/block-service"
+"use server";
+import { getSelf } from "@/lib/auth-service";
+import { blockUser, unblockUser } from "@/lib/block-service";
+import { RoomServiceClient } from "livekit-server-sdk";
 import { revalidatePath } from "next/cache";
 
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!
+);
+
 export const onBlock = async (id: string) => {
+  const self = await getSelf();
 
-    const blockedUser = await blockUser(id);
+  let blockedUser;
 
-    revalidatePath('/')
+  try {
+    blockedUser = await blockUser(id);
+  } catch (error) {
+    // uer is guest
+  }
 
-    if(blockedUser) revalidatePath(`/${blockedUser.blocked.username}`)
+  try {
+    await roomService.removeParticipant(self.id, id);
+  } catch (error) {
+    // user is not in room
+  }
 
-    return blockedUser
-}
+  revalidatePath(`/u/${self.username}/community`);
+
+  return blockedUser;
+};
 
 export const onUnblock = async (id: string) => {
+  const unBlockedUser = await unblockUser(id);
 
-    const unBlockedUser = await unblockUser(id);
+  revalidatePath("/");
 
-    revalidatePath('/')
+  if (unBlockedUser) revalidatePath(`/${unBlockedUser.blocked.username}`);
 
-    if(unBlockedUser) revalidatePath(`/${unBlockedUser.blocked.username}`)
-
-    return unBlockedUser
-}
+  return unBlockedUser;
+};
--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -1,8 +1,8 @@
+import StreamPlayer from "@/components/stream-player";
+import { isBlockedByUser } from "@/lib/block-service";
 import { isFollowingUser } from "@/lib/follow-service";
 import { getUserByUserName } from "@/lib/user-service";
 import { notFound } from "next/navigation";
-import { Actions } from "./_components/actions";
-import { isBlockedByUser } from "@/lib/block-service";
 
 interface UsernamePageProps {
   params: {
@@ -11,22 +11,18 @@ interface UsernamePageProps {
 }
 
 const UserPage = async ({ params }: UsernamePageProps) => {
+  
   const user = await getUserByUserName(params.username);
 
-  if (!user) notFound();
+  if (!user || !user.stream) notFound();
 
   const isFollowing = await isFollowingUser(user.id);
-  const isBlocked = await isBlockedByUser(user.id)
+  const isBlocked = await isBlockedByUser(user.id);
+
+  if (isBlocked) notFound();
 
   return (
-    <div className="">
-      <p>{user.username}</p>
-      <p>{user.id}</p>
-      <p>{user.externalUserId}</p>
-      <p>is following:{`${isFollowing}`}</p>
-      <p>{`is blocked: ${isBlocked} `}</p>
-      <Actions userId={user.id} isFollowing={isFollowing} />
-    </div>
+    <StreamPlayer user={user} stream={user.stream} isFollowing={isFollowing} />
   );
 };
 
